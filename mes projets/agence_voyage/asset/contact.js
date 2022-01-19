2b173b5cdd04dbb310d9240bcba8656258565cb5
JulienGenonceau <juliengenonceau@hotmail.com>
2022-01-19 11:30:17 +0100

    image

---
+++ mes projets/agence_voyage/asset/contact.js
@@ -0,0 +1,28 @@
+const email = document.getElementById('email')
+var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+function test() {
+    ValidateEmail(email)
+}
+
+function ValidateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+    if (input.value.match(validRegex)) {
+  
+      alert("Valid email address!");
+  
+      return true;
+  
+    } else {
+  
+      alert("Invalid email address!");
+  
+      return false;
+  
+    }
+  
+  }
+  
+  D
